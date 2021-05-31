afff8d4739a9da270eab06593eeaa9ef0f6b4ba8
Igneel001 <aniruddhan.suresh@gmail.com>
2021-05-31 11:50:13 +0530

    added system tray icon

--- tracker.py
+++ tracker.py
@@ -1,6 +1,16 @@
-import pynput
+import sys
 from pynput import keyboard
+from PyQt5.QtGui import * 
+from PyQt5.QtWidgets import * 
 
+# system tray icon
+app = QApplication(sys.argv)
+
+trayIcon = QSystemTrayIcon(QIcon('icon.png'), parent=app)
+trayIcon.setToolTip('Typing Speed Tracker')
+trayIcon.show()
+
+# keylogging
 count = 0
 
 def on_press(key):
@@ -14,10 +24,11 @@ def on_press(key):
             count += 1
             print(count)
 
-
 def on_release(key):
     if key == keyboard.Key.insert:
         return False
 
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
+sys.exit(app.exec_())
