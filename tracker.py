20b03b10b672db88f0b1ab7c1c1c9f0efb82ade4
Igneel001 <aniruddhan.suresh@gmail.com>
2021-06-01 16:08:59 +0530

    better way of handling if statement

--- tracker.py
+++ tracker.py
@@ -13,10 +13,22 @@ from PyQt5.QtWidgets import *
 keystrokes = []
 keystoke_count = 0
 
+ignored_keystrokes = [
+                        keyboard.Key.f12, keyboard.Key.f11, keyboard.Key.f10, keyboard.Key.f9, keyboard.Key.f8, 
+                        keyboard.Key.f7, keyboard.Key.f6, keyboard.Key.f5, keyboard.Key.f4, keyboard.Key.f3, 
+                        keyboard.Key.f2, keyboard.Key.f1, keyboard.Key.print_screen, keyboard.Key.media_play_pause,
+                        keyboard.Key.media_volume_up, keyboard.Key.media_volume_down, keyboard.Key.media_volume_mute, 
+                        keyboard.Key.space, keyboard.Key.enter, keyboard.Key.insert, keyboard.Key.tab, keyboard.Key.alt,
+                        keyboard.Key.delete, keyboard.Key.end, keyboard.Key.home, keyboard.Key.esc, keyboard.Key.caps_lock,
+                        keyboard.Key.shift_r, keyboard.Key.shift_l, keyboard.Key.ctrl_l, keyboard.Key.ctrl_r, keyboard.Key.alt_l,
+                        keyboard.Key.alt_gr, keyboard.Key.cmd, keyboard.Key.page_down, keyboard.Key.page_up, keyboard.Key.left, 
+                        keyboard.Key.right, keyboard.Key.down, keyboard.Key.up
+                    ]
+
 def on_press(key):
     global keystoke_count
         
-    if key == keyboard.Key.f12 or key == keyboard.Key.f11 or key == keyboard.Key.f10 or key == keyboard.Key.f9 or key == keyboard.Key.f8 or key == keyboard.Key.f7 or key == keyboard.Key.f6 or key == keyboard.Key.f5 or key == keyboard.Key.f4 or key == keyboard.Key.f3 or key == keyboard.Key.f2 or key == keyboard.Key.f1 or key == keyboard.Key.print_screen or key == keyboard.Key.media_play_pause or key == keyboard.Key.media_volume_up or key == keyboard.Key.media_volume_down or key == keyboard.Key.media_volume_mute or key == keyboard.Key.space or key == keyboard.Key.enter or key == keyboard.Key.insert or key == keyboard.Key.tab or key == keyboard.Key.alt or key == keyboard.Key.delete or key == keyboard.Key.end or key == keyboard.Key.home or key == keyboard.Key.esc or key == keyboard.Key.caps_lock or key == keyboard.Key.shift_r or key == keyboard.Key.shift_l or key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r or key == keyboard.Key.alt_l or key == keyboard.Key.alt_gr or key == keyboard.Key.cmd or key == keyboard.Key.page_down or key == keyboard.Key.page_up or key == keyboard.Key.left or key == keyboard.Key.right or key == keyboard.Key.down or key == keyboard.Key.up:
+    if key in ignored_keystrokes:
         pass
     
     elif key == keyboard.Key.backspace:
