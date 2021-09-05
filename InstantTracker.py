dc34c9658cb1224083ce6feea0bc7d73eb2c6c6a
Igneel001 <aniruddhan.suresh@gmail.com>
2021-09-05 18:45:05 +0530

    added required 'bling'

--- InstantTracker.py
+++ InstantTracker.py
@@ -1,39 +1,26 @@
-import sys
+# IMPORTS
 import time
 import threading
 from pynput import keyboard
-from PyQt5.QtGui import * 
-from PyQt5.QtWidgets import * 
 
 
+# ASCII ART
+print(" _____                 __  __      _")
+print("|_   _|   _ _ __   ___|  \/  | ___| |_ ___ _ __")
+print("  | || | | | '_ \ / _ \ |\/| |/ _ \ __/ _ \ '__|")
+print("  | || |_| | |_) |  __/ |  | |  __/ ||  __/ |")
+print("  |_| \__, | .__/ \___|_|  |_|\___|\__\___|_|")
+print("      |___/|_|")
+print("\n Press insert to exit the program\n")
+
+# GLOBAL VARIABLES
 keystrokeCount = 0
 wordCount = 1
 tempCount = 0
 avgWpm = 0
-"""
-Removed: 
-clearCount,Keystrokes in main
-parameter words in func wpm
-snippet to clear keystrokes
-"""
 
-"""
-Added:
-AND COMMENTED lpm var in func wpm (It seems a little odd to display)
-wordStartTime in main
-TimePeriod in func on_release
-changed avg letter count to become a word as 5 instead of 4(The if condition)
 
-"""
-
-"""
-ignoredKeystrokes:
-
-ERROR
-
- keyboard.Key.prinont_screen, keyboard.Key.media_play_pause,
-"""
-
+# KEYSTROKES TO BE IGNORED
 ignoredKeystrokes = [
                         keyboard.Key.f12, keyboard.Key.f11, keyboard.Key.f10, keyboard.Key.f9, keyboard.Key.f8, 
                         keyboard.Key.f7, keyboard.Key.f6, keyboard.Key.f5, keyboard.Key.f4, keyboard.Key.f3,                         
@@ -47,6 +34,8 @@ ignoredKeystrokes = [
 
 startTime = wordStartTime = endTime = -1.0
 
+
+# MAIN ALGORITHM
 def on_press(key):
     global keystrokeCount, wordCount, tempCount,  wordStartTime,startTime,endTime
 
@@ -85,25 +74,28 @@ def on_press(key):
         t = threading.Timer(executionTime, wpm, [executionTime])
         t.start()
 
+
+# DISPLAY WPM
 def wpm(TimePeriod):
     global wordCount,avgWpm
     wpm = 60 / TimePeriod
-    if avgWpm==0:
+    if avgWpm == 0:
         avgWpm=wpm 
     else:
         avgWpm = (avgWpm+wpm)/2
-    #lpm = 60*4*(1/TimePeriod)
-    print("WPM: ", round(wpm))
-    print("Avg WPM: ",round(avgWpm))
+    print("WPM: ",round(avgWpm))
+    
 
+# END PROGRAM
 def on_release(key):
     TimePeriod = endTime-startTime
     if key == keyboard.Key.insert:
-        print("Word count:      ", wordCount)
-        print("Keystroke count: ", keystrokeCount)
+        print("Session duration: ", round(TimePeriod), "seconds")
         print("Avg words per min: ",round(wordCount*(60/TimePeriod)))
         print("Avg letters per min:", round(keystrokeCount*(60/TimePeriod)))
         return False
 
+
+# EVENT LISTENER
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
